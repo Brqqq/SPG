3a4401b1f4744860be6527b84d6a180db86fd851
Brqqq <b.karatas0@gmail.com>
2017-04-17 02:19:01 +0200

    Made it so that a new wallpaper is used right away, instead of waiting for the first interval timer to pass

--- SPG/app.js
+++ SPG/app.js
@@ -7,11 +7,12 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 const tick = function () {
-    setInterval(() => {
+    setInterval(function next() {
         imageManager.getNextImageURL(() => {
             wallpaper.set('image.png');
         });
-    }, config.delay * 1000 * 60);
+        return next;
+    }(), config.delay * 1000 * 60);
 }
 
 const imageManager = new ImageManager(config, tick);
